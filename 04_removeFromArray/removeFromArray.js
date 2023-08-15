882654fe8a1472004ae569db1024dc22cbaad83e
michaelma7 <122073103+michaelma7@users.noreply.github.com>
2023-08-15 14:02:52 -0400

    exercise 4
    created loop that removed values from given array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...removes) {
+    //find index of all items to remove
+    //loop through each index and remove item
+    for( const remove of removes) {
+        let index = array.indexOf(remove);
+        while (index !== -1) {
+            array.splice(index, 1);
+            index = array.indexOf(remove, index++);
+        }
+    }
+    return array;  
 };
 
 // Do not edit below this line
