848326d374592f600847816134bf5b32359165a7
michaelma7 <122073103+michaelma7@users.noreply.github.com>
2023-08-15 10:12:59 -0400

    exercise 2
    created function to return a string of a word repeated x times

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(text, num) {
+    let sentence = '';
+    if (num < 0) {
+        return "ERROR";
+    }
+    for(let i = num; i > 0; i--) {
+        sentence += text;
+    };
+    return sentence;
 };
 
 // Do not edit below this line
