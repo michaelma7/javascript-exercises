ac37debab267b0716611e5fb2d792086517f07fd
michaelma7 <122073103+michaelma7@users.noreply.github.com>
2023-08-15 16:38:20 -0400

    Exercise 5
    created sum all function that adds all numbers between two given

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,30 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    //check that passed values are positive numbers
+    let larger;
+    let smaller;
+    if (typeof(num1) != typeof(1) || typeof(num2) != typeof(1) || num1 < 0 || num2 < 0){
+        return "ERROR";
+    }
+    // determine which number is larger
+    else if (num1 > num2) {
+        larger = num1;
+        smaller = num2;
+    }
+    else {
+        larger = num2;
+        smaller = num1;
+    }
+    //create array with numbers between numbers
+    let numbers = [];
+    for(let i = smaller; i <= larger; i++) {
+        numbers.push(i);
+    }
+    //sum all values with reduce
+    function add (total, num) {
+        return total + num;
+    }
+    let sum = numbers.reduce(add, 0);
+    return sum;
 };
 
 // Do not edit below this line
