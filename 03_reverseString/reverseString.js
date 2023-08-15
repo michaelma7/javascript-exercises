6ade0fbe50e8f7d778ddf10be796443ee0120b2d
michaelma7 <122073103+michaelma7@users.noreply.github.com>
2023-08-15 11:44:20 -0400

    exercise 3
    reverseString function w/o loops
    does not work with commas
    will update for that

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(text) {
+    let separated = text.split("");
+    let reversed = separated.reverse();
+    let reversedString = reversed.toString();
+    let completed = reversedString.replaceAll(",","");
+    return completed;
 };
 
 // Do not edit below this line
