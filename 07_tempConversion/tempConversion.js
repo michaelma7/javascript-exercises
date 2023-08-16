f9b0d4bc5b24f324521c5207159446125b742c41
michaelma7 <122073103+michaelma7@users.noreply.github.com>
2023-08-15 20:53:52 -0400

    exercise 7
    used parseFloat and toFixed to get one decimal accuracy on   conversioin

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,19 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  if (isNaN(temp)) {
+    return "Error";
+  }
+  celsius = (temp -32) * 5/9;
+  rounded = parseFloat(celsius.toFixed(1));
+  return rounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  if (isNaN(temp)) {
+    return "Error";
+  }
+  fahrenheit = (temp *9/5) +32;
+  rounded = parseFloat(fahrenheit.toFixed(1));
+  return rounded;
 };
 
 // Do not edit below this line
